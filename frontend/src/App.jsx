de4678bf1c7b677f7fc099f89fa431392f328afb
BIN BIN <thangpd300502@gmail.com>
2025-05-07 13:57:22 +0700

    ROUTER & TOASTER

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,29 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import OnboardingPage from "./pages/OnboardingPage";
+import SignInPage from "./pages/SignInPage";
+import NotificationsPage from "./pages/NotificationsPage";
+import ChatPage from "./pages/ChatPage";
+import CallPage from "./pages/CallPage";
+import { Toaster } from "react-hot-toast";
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/signup" element={<SignUpPage />} />
+				<Route path="/onboarding" element={<OnboardingPage />} />
+				<Route path="/signin" element={<SignInPage />} />
+				<Route path="/notifications" element={<NotificationsPage />} />
+				<Route path="/chat" element={<ChatPage />} />
+				<Route path="/call" element={<CallPage />} />
+			</Routes>
+
+			<Toaster />
+		</div>
+	);
 };
 
 export default App;
