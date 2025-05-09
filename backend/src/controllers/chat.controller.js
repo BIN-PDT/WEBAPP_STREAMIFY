e2fcf28c8330a7bdaad568241c6e46020bec9c70
BIN BIN <thangpd300502@gmail.com>
2025-05-09 09:48:44 +0700

    BACKEND FIX UP

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -4,7 +4,7 @@ import { generateStreamToken } from "../utils/stream.util.js";
 export function getStreamToken(req, res) {
 	const { user } = req;
 
-	const token = generateStreamToken(user.id);
+	const { data: token } = generateStreamToken(user.id);
 
 	return new APIResponse(200).setData({ token }).send(res);
 }
